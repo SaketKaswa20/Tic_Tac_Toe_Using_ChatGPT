7988db906cd30e680c9a02891b4e0ed3162f0578
Saket Kaswa <105808363+SaketKaswa20@users.noreply.github.com>
2023-09-28 19:22:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,21 +5,25 @@ document.addEventListener('DOMContentLoaded', () => {
   const winnerPopup = document.getElementById('winner-popup');
   const popupMessage = document.getElementById('popup-message');
   const startNewGameButton = document.getElementById('start-new-game-button');
+  const undoMoveButton = document.getElementById('undo-move-button');
 
   let currentPlayer = 'X';
   let gameEnded = false;
+  let moveHistory = [];
 
-  cells.forEach(cell => {
-    cell.addEventListener('click', handleCellClick);
+  cells.forEach((cell, index) => {
+    cell.addEventListener('click', () => handleCellClick(cell, index));
   });
 
   restartButton.addEventListener('click', restartGame);
+  undoMoveButton.addEventListener('click', undoMove);
 
-  function handleCellClick() {
-    if (!gameEnded && this.innerHTML === '') {
-      this.innerHTML = currentPlayer;
-      this.style.backgroundColor = currentPlayer === 'X' ? '#fcd462' : '#77dd77';
-      this.style.color = currentPlayer === 'X' ? '#1f2735' : '#1f2735';
+  function handleCellClick(cell, index) {
+    if (!gameEnded && cell.innerHTML === '') {
+      moveHistory.push({ index, player: currentPlayer });
+      cell.innerHTML = currentPlayer;
+      cell.style.backgroundColor = currentPlayer === 'X' ? '#fcd462' : '#77dd77';
+      cell.style.color = currentPlayer === 'X' ? '#1f2735' : '#1f2735';
       currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
 
       setTimeout(() => {
@@ -55,20 +59,26 @@ document.addEventListener('DOMContentLoaded', () => {
         // Display the winner's popup message
         popupMessage.textContent = `Player ${cellA} wins!`;
         winnerPopup.style.display = 'flex';
-      } else{
-        popupMessage.textContent= 'It's a Tie';
-          winnerPopup.style.display='flex;
       }
     });
+
+    if (!gameEnded && [...cells].every(cell => cell.innerHTML !== '')) {
+      // Game is a tie
+      popupMessage.textContent = "It's a tie!";
+      winnerPopup.style.display = 'flex';
+    }
   }
 
-  startNewGameButton.addEventListener('click', () => {
-    // Hide the winner's popup
-    winnerPopup.style.display = 'none';
-
-    // Restart the game
-    restartGame();
-  });
+  function undoMove() {
+    if (!gameEnded && moveHistory.length > 0) {
+      const lastMove = moveHistory.pop();
+      const cell = cells[lastMove.index];
+      cell.innerHTML = '';
+      cell.style.backgroundColor = '#303b4a';
+      cell.style.color = '#fff';
+      currentPlayer = lastMove.player;
+    }
+  }
 
   function restartGame() {
     cells.forEach(cell => {
@@ -80,5 +90,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     currentPlayer = 'X';
     gameEnded = false;
+    moveHistory = [];
+
+    // Hide the winner's popup
+    winnerPopup.style.display = 'none';
   }
 });
